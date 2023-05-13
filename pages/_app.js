5fec30cd7dd000cec107adb5876633bf71d839bc
Ilya Dorman <whitestar.i8@gmail.com>
2023-05-13 17:05:16 +0100

    basic setup works

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
 import '@/styles/globals.css'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { mainnet, polygon } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+import { LensConfig, development } from '@lens-protocol/react-web'
+import { bindings as wagmiBindings } from '@lens-protocol/wagmi'
+import { LensProvider } from '@lens-protocol/react-web'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const { provider, webSocketProvider } = configureChains([polygon, mainnet], [publicProvider()])
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider
+})
+
+const lensConfig = {
+  bindings: wagmiBindings(),
+  environment: development
+}
+
+
+export default function App ({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={client}>
+      <LensProvider config={lensConfig}>
+        <Component {...pageProps} />
+      </LensProvider>
+    </WagmiConfig>
+  )
 }
