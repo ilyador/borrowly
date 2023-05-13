c120f5839ef6d334637e2e04cdc7a33069ca7700
Ilya Dorman <whitestar.i8@gmail.com>
2023-05-13 15:11:41 +0100

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { ethers } from 'ethers'
 import { useState } from 'react'
 
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home () {
   const [errorMessage, setErrorMessage] = useState(null)
@@ -48,7 +46,7 @@ export default function Home () {
         <meta name='viewport' content='width=device-width, initial-scale=1'/>
         <link rel='icon' href='/favicon.ico'/>
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={styles.main}>
         <h1>HELLO</h1>
 
         <button onClick={connectWalletHandler}>CONNECT</button>
